a2948c58b45c0ce9c7d4267cc7e432fc1494b46f
JanSamuel <36290290+JanSamuel@users.noreply.github.com>
2018-02-13 15:11:13 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 
 int main()
 {
-std::cout<<"Hello \nthis is Quiztronic 3000 game made for you to play with your friends \non one computer like old times with 'hot seat'"<<std::endl;
-std::srand((unsigned int)time(0));
-Quiz game;
-game.how_many_players();
-game.game();
-return 0;
+    std::cout<<"Hello \nthis is Quiztronic 3000 game made for you to play with your friends \non one computer like old times with 'hot seat'"<<std::endl;
+    std::srand((unsigned int)time(0));
+    Quiz game;
+    game.how_many_players();
+    game.game();
+    return 0;
 }
